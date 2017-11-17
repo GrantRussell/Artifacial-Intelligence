304cb767c621f51a03d4ddb8185e4ea3d3cf2477
Jacob <jacob.m.adams@gmail.com>
2017-11-17 15:09:41 -0700

    IMAGES: added images to db

---
+++ setup/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 3,'/assets/images/surprised-cute-kitten.jpg');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 4,'/assets/images/angry__605.jpg');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 1,'/assets/images/sad-baby-kittens.jpg');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 1,'/assets/images/happy-cute-kitten-pictures.jpg');
---
+++ setup/init.sql
@@ -0,0 +1,10 @@
+INSERT INTO type (type_name) VALUES ('video');
+INSERT INTO type (type_name) VALUES ('image');
+INSERT INTO type (type_name) VALUES ('gif');
+
+INSERT INTO emotion (emotion) VALUES ('sad');
+INSERT INTO emotion (emotion) VALUES ('happy');
+INSERT INTO emotion (emotion) VALUES ('surprised');
+INSERT INTO emotion (emotion) VALUES ('angry');
+INSERT INTO emotion (emotion) VALUES ('disgust');
+INSERT INTO emotion (emotion) VALUES ('fear');
