a2941520ae0b0d005cd90d7abd7adda4e7e4161b
Parker Petersen <parkpetersen@gmail.com>
2017-11-18 16:51:51 -0700

    sql queries

--- setup/data.sql
+++ setup/data.sql
@@ -2,3 +2,17 @@ INSERT INTO media (type_id, emotion_id, url) VALUES (2, 3,'/assets/images/surpri
 INSERT INTO media (type_id, emotion_id, url) VALUES (2, 4,'/assets/images/angry__605.jpg');
 INSERT INTO media (type_id, emotion_id, url) VALUES (2, 1,'/assets/images/sad-baby-kittens.jpg');
 INSERT INTO media (type_id, emotion_id, url) VALUES (2, 1,'/assets/images/happy-cute-kitten-pictures.jpg');
+-- All below are new queries
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 2, 'https://media1.tenor.com/images/efc9f6ce8923eadf351b676f29bf943c/tenor.gif?itemid=5449984');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 2, 'https://media3.giphy.com/media/SggILpMXO7Xt6/200.webp#2-grid1');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 1, 'https://media2.giphy.com/media/NKTk26o6xzNFm/200w.webp#4-grid1');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 2, 'https://media1.giphy.com/media/26tP3M3i03hoIYL6M/200.webp#8-grid1');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 2, 'https://media0.giphy.com/media/nfLpqTrNPpqcE/200.webp#14-grid1');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 2, 'https://media3.giphy.com/media/f1GG38RQLvdgQ/200.webp#35-grid1');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 2, 'https://media3.giphy.com/media/CJq3gz9LItpZu/200.webp#0-grid1');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 2, 'https://i.ytimg.com/vi/daoTiDaCLg8/maxresdefault.jpg');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 2, 'https://i.imgur.com/MeZR6qb.jpg');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 2, 'https://i.imgur.com/AUqtZha.jpg');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 1, 'https://i.imgur.com/UaTvr2U.jpg');
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 1, 'https://i.imgur.com/TZpiaIMg.jpg);
+INSERT INTO media (type_id, emotion_id, url) VALUES (2, 1, 'https://i.imgur.com/FwtTklR.jpg');
