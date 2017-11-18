e8d5e837f9717aa791763e4baaaa5eb7bbc92878
GrantRussell <grant.s.russ@gmail.com>
2017-11-18 02:47:46 -0700

    changed server.js to express from html, implemented file upload

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
-const http = require('http');
-const serveStatic = require('serve-static');
-const finalhandler = require('finalhandler');
+// const http = require('http');
+// const serveStatic = require('serve-static');
+// const finalhandler = require('finalhandler');
+//
+// var serve = serveStatic('app/', {'index': ['index.html', 'index.htm']});
+//
+// var server = http.createServer(function onRequest (req, res) {
+//   serve(req, res, finalhandler(req, res))
+// });
+//
+// console.log("Listening on 3000");
+//
+// // Listen
+// server.listen(3000);
 
-var serve = serveStatic('app/', {'index': ['index.html', 'index.htm']});
+var path = require("path");
+var express = require('express'), multer = require('multer'), app = express(), port = 3000;
+app.set('port', port);
 
-var server = http.createServer(function onRequest (req, res) {
-  serve(req, res, finalhandler(req, res))
+var storage = multer.diskStorage({
+  destination: function (request, file, callback) {
+    callback(null, '/upload/');
+  },
+  filename: function (request, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      if (err) return cb(err)
+      cb(null, raw.toString('hex') + path.extname(file.originalname))
+    })
+  }
 });
 
-console.log("Listening on 3000");
+app.use(express.static('app/'));
 
-// Listen
-server.listen(3000);
+var upload = multer( { dest: 'upload/' } );
+
+app.get('/', function(request, response) {
+  response.sendFile('/index.html');
+});
+
+app.post( '/upload', upload.single( 'file' ), function( req, res, next ) {
+  return res.status(200).send(req.file);
+});
+
+var server = app.listen(port, function () {
+  console.log('Listening on port ' + server.address().port)
+});
